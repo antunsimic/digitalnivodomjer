b0a603c479b07eafd4198750137dace2cd187893
antunsimic <95712724+antunsimic@users.noreply.github.com>
2024-04-04 16:35:48 +0200

    Ispravak sitnih grešaka bankovni_izvodi.py
    Nekim parsiranim vrijednostima odsijecao se zadnji znak. Sad se ispravno uzima cijela potrebna vrijednost.

--- bankovni_izvodi.py
+++ bankovni_izvodi.py
@@ -18,7 +18,7 @@ redniBrojStavkeIzvoda = 0
 for line in readLines:
     flag = str(line[len(line)-4]) + str(line[len(line)-3]) + str(line[len(line)-2])
     if(flag == '903'):
-        for x in range(166, 168):
+        for x in range(166, 169):
             redniBrojIzvoda += str(line[x])
         
         for x in range(172, 180):
@@ -26,42 +26,42 @@ for line in readLines:
 
     if(flag == '905'):
         tmp = ''
-        for x in range(2, 35):
+        for x in range(2, 36):
             tmp += str(line[x])
         racunPlatitelja.append(tmp)
 
         tmp = ''
-        for x in range(36, 105):
+        for x in range(36, 106):
             tmp += str(line[x])
         nazivPlatitelja.append(tmp)
 
         tmp = ''
-        for x in range(106, 140):
+        for x in range(106, 141):
             tmp += str(line[x])
         adresaPlatitelja.append(tmp)
         
         tmp = ''
-        for x in range(141, 175):
+        for x in range(141, 176):
             tmp += str(line[x])
         sjedistePlatitelja.append(tmp)
         
         tmp = ''
-        for x in range(184, 191):
+        for x in range(184, 192):
             tmp += str(line[x])
         datumIzvrsenja.append(tmp)
 
         tmp = ''
-        for x in range(227, 241):
+        for x in range(227, 242):
             tmp += str(line[x])
         iznos.append(tmp)
 
         tmp = ''
-        for x in range(268, 293):
+        for x in range(268, 294):
             tmp += str(line[x])
         pozivNaBrojPlatitelja.append(tmp)
 
         tmp = ''
-        for x in range(298, 437):
+        for x in range(298, 438):
             tmp += str(line[x])
         opisPlacanja.append(tmp)
 

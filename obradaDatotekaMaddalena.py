5d3b0442c66c7d5c1eb66122485124e14ee556e9
fmartinkovic <filip.martinkovic2@gmail.com>
2024-04-08 19:31:40 +0200

    Maddalena - Izračun potrošnje prethodnog mjeseca

--- obradaDatotekaMaddalena.py
+++ obradaDatotekaMaddalena.py
@@ -4,13 +4,13 @@ import sqlite3
 from datetime import datetime
 
 class Maddalena:
-    def __init__(self, broj_rmodul, datum_tren, stanje_tren, stanje_preth_mj, datum_m3):
+    def __init__(self, broj_rmodul, datum_tren, stanje_tren, stanje_preth_mj, datum_m3, potrosnja_preth_mj):
         self.broj_rmodul = broj_rmodul
         self.datum_tren = datum_tren
-        self.stanje_tren = stanje_tren
-        self.stanje_preth_mj = stanje_preth_mj
+        self.stanje_tren = int(float(stanje_tren)*1000)
+        self.stanje_preth_mj = int(float(stanje_preth_mj)*1000)
         self.datum_m3 = datum_m3
-        #izracun ovdje mozda?
+        self.potrosnja_preth_mj = potrosnja_preth_mj
 
 data_frames = []
 excel_files = [file for file in os.listdir() if file.endswith('.xlsx')]  # sve excel datoteke u trenutnom direktoriju
@@ -39,22 +39,45 @@ for file in excel_files:
 
 svi_podaci = [] #pandas je glup
 
+conn = sqlite3.connect('vodomjeri.db')
+
+cur = conn.cursor()
+
 #iteriraj kroz frameove; dodaj
 for frame in data_frames:
     #iterirati od 0 do 29 (valjda je to max broj unosa)
     for i in range(28):
+
+        #ovdje trazimo podatke za racunanje potrosnja_preth_mj -> stanje_preth_mj je value7, to imamo (trenutni datum 4.4)
+        #racunamo datum_m3 od trenutnog datum_m3                                                      (trenutni datum_m3 31.3)
+        #kad znamo datum_m3_inception onda uz broj modula najdemo trenutnu potrosnju                  (racunamo datum_m3 od 31.3 = 28/29. 2., TU JE RJESENJE)
+
+        
+        datum_m3 = pd.to_datetime(frame['Datum_m3'][i], format="%d.%m.%Y")
+        datum_m3_m3 = (datum_m3 - pd.offsets.MonthEnd(1))
+        datum_m3_m3 = datum_m3_m3.strftime('%d.%m.%Y')
+
+        cur.execute(f"SELECT Stanje_tren FROM Ocitanje WHERE Broj_rmodul={frame['Module serial'][i]} AND Datum_preth_mj='{datum_m3_m3}'")
+
+        stanje = cur.fetchone()
+        if stanje is not None:
+            stanje_predzadnje_ocitanje = stanje[0]
+        else:
+            stanje_predzadnje_ocitanje = 0
+
+
+        potrosnja_preth_mj = float(float(frame['Value7'][i])*1000 - stanje_predzadnje_ocitanje) / 1000
+
+        print("{:.2f}".format(potrosnja_preth_mj))
+
         #print(frame['Module serial'][i], frame['Timestamp'][i], frame['Reading'][i], frame['Value7'][i])
-        svi_podaci.append(Maddalena(frame['Module serial'][i], frame['Timestamp'][i], frame['Reading'][i], frame['Value7'][i], frame['Datum_m3'][i]))
+        svi_podaci.append(Maddalena(frame['Module serial'][i], frame['Timestamp'][i], frame['Reading'][i], frame['Value7'][i], frame['Datum_m3'][i], potrosnja_preth_mj))
 
-
-
-conn = sqlite3.connect('vodomjeri.db')
-#conn.execute()
 
 
 for unos in svi_podaci:
-                                            #INTEGER   #STRING     #FLOAT         #FLOAT         #STRING
-    conn.execute(f"INSERT INTO Ocitanje (Broj_rmodul, Datum_tren, Stanje_tren, Stanje_preth_mj, Datum_preth_mj) VALUES ({unos.broj_rmodul}, '{unos.datum_tren}', {unos.stanje_tren}, {unos.stanje_preth_mj}, '{unos.datum_m3}')")
+                                            #INTEGER   #STRING     #INTEGER         #FLOAT         #STRING
+    cur.execute(f"INSERT INTO Ocitanje (Broj_rmodul, Datum_tren, Stanje_tren, Stanje_preth_mj, Datum_preth_mj, Potrosnja_preth_mj) VALUES ({unos.broj_rmodul}, '{unos.datum_tren}', {unos.stanje_tren}, {unos.stanje_preth_mj}, '{unos.datum_m3}', {unos.potrosnja_preth_mj})")
 
 conn.commit()
 conn.close()

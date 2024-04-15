a1b695b1f3547aa93e93f5ffffbaf79b46466614
Rista55 <93721542+Rista55@users.noreply.github.com>
2024-04-15 21:52:14 +0200

    Izvještaj zgrada
    Popravljen kod. Za pokretanje programa pr.: python GeneriranjeIzvjestajaZaVodovod.py 202402 18

--- GeneriranjeIzvjestajaZaVodovod.py
+++ GeneriranjeIzvjestajaZaVodovod.py
@@ -1,25 +1,45 @@
 import sqlite3
+import sys
+
+#provjera jesu li navedeni argumenti
+if len(sys.argv) != 3:
+    print("Koristite: python program.py <razdoblje_obracun> <id_zgrada>")
+    sys.exit(1)
+
+razdoblje_obracun = sys.argv[1]
+id_zgrada = sys.argv[2]
 
 conn = sqlite3.connect('vodomjeri.db')
 cursor = conn.cursor()
 
-#dohvaćanje svih id-eva zgrada
-cursor.execute('SELECT ID_zgrada FROM Zgrada')
-building_ids = [row[0] for row in cursor.fetchall()]
+#provjera postoje li potrebni podaci u bazi obracun
+cursor.execute('''
+    SELECT O.*
+    FROM Obracun O
+    JOIN Korisnik K ON O.ID_korisnik = K.ID_korisnik
+    WHERE O.Razdoblje_obracun = ?
+    AND K.ID_zgrada = ?;
+''', (razdoblje_obracun, id_zgrada))
 
-#prolazak kroz svaku zgradu i dohvaćanje podataka o korisnicima za trenutnu zgradu
-for zgrada_id in building_ids:
-    print(f"Izvještaj za zgradu s ID-om {zgrada_id}:")
-    print("ID\tŠifra korisnika\tŠifra MM\tIme i prezime\tPotrošnja HV")
-    cursor.execute('''
-        SELECT Korisnik.ID_korisnik, Korisnik.Vod_sif_kor, Korisnik.Vod_mm, Korisnik.Ime, Korisnik.Prezime, Obracun.Potrosnja_hv
-        FROM Korisnik
-        INNER JOIN Obracun ON Korisnik.ID_korisnik = Obracun.ID_korisnik
-        WHERE Korisnik.ID_zgrada = ?
-    ''', (zgrada_id,))
-    
-    #ispis podataka o korisnicima
-    for data in cursor.fetchall():
-        print(f"{data[0]}\t{data[1]}\t{data[2]}\t{data[3]} {data[4]}\t{data[5]}")
+if not cursor.fetchall():
+    print("Nema dostupnih podataka za navedeno razdoblje obračuna i ID zgrade.")
+    conn.close()
+    sys.exit(0)
+
+print(f"Izvještaj za zgradu s ID-om {id_zgrada}:")
+print("ID\tŠifra korisnika\tŠifra MM\tIme i prezime\tPotrošnja HV\tPotrošnja SV\tRazlika")
+
+#dohvaćanje podataka o korisnicima za odabrano razdoblje obračuna i zgradu
+cursor.execute('''
+    SELECT Korisnik.ID_korisnik, Korisnik.Vod_sif_kor, Korisnik.Vod_mm, Korisnik.Ime, Korisnik.Prezime, Obracun.Potrosnja_hv
+    FROM Korisnik
+    JOIN Obracun ON Korisnik.ID_korisnik = Obracun.ID_korisnik
+    WHERE Korisnik.ID_zgrada = ?
+    AND Obracun.Razdoblje_obracun = ?;
+''', (id_zgrada, razdoblje_obracun))
+
+#ispis podataka o korisnicima
+for data in cursor.fetchall():
+    print(f"{data[0]}\t{data[1]}\t{data[2]}\t{data[3]} {data[4]}\t{data[5]}\t0\t0")
 
 conn.close()

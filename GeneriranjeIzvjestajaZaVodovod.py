3c9e68e6da3581927941433a7271406414f6b2f4
Rista55 <93721542+Rista55@users.noreply.github.com>
2024-04-16 19:29:37 +0200

    Popravljen kod
    Dohvaća najnoviji Razdoblje_obracun i generira izvještaj za sve zgrade ako postoji podatak Razdoblje_obracun za korisnika. Pretpostavljam da baza Obracun uvijek ima bar neke podatke za Razdoblje_obracun, inače ako je baza Obracun, Razdoblje_obracun prazno onda dolazi do greške u programu.

--- GeneriranjeIzvjestajaZaVodovod.py
+++ GeneriranjeIzvjestajaZaVodovod.py
@@ -1,45 +1,36 @@
 import sqlite3
 import sys
 
-#provjera jesu li navedeni argumenti
-if len(sys.argv) != 3:
-    print("Koristite: python program.py <razdoblje_obracun> <id_zgrada>")
+if len(sys.argv) != 1:
+    print("Koristite: python program.py")
     sys.exit(1)
-
-razdoblje_obracun = sys.argv[1]
-id_zgrada = sys.argv[2]
 
 conn = sqlite3.connect('vodomjeri.db')
 cursor = conn.cursor()
 
-#provjera postoje li potrebni podaci u bazi obracun
-cursor.execute('''
-    SELECT O.*
-    FROM Obracun O
-    JOIN Korisnik K ON O.ID_korisnik = K.ID_korisnik
-    WHERE O.Razdoblje_obracun = ?
-    AND K.ID_zgrada = ?;
-''', (razdoblje_obracun, id_zgrada))
+#dohvaćanje najnovijeg Razdoblje_obracun
+cursor.execute('SELECT MAX(Razdoblje_obracun) FROM Obracun')
+najnovije_razdoblje = cursor.fetchone()[0]
 
-if not cursor.fetchall():
-    print("Nema dostupnih podataka za navedeno razdoblje obračuna i ID zgrade.")
-    conn.close()
-    sys.exit(0)
+#dohvaćanje svih ID_zgrada
+cursor.execute('SELECT DISTINCT ID_zgrada FROM Korisnik')
+zgrade = cursor.fetchall()
 
-print(f"Izvještaj za zgradu s ID-om {id_zgrada}:")
-print("ID\tŠifra korisnika\tŠifra MM\tIme i prezime\tPotrošnja HV\tPotrošnja SV\tRazlika")
+for zgrada_id, in zgrade:
+    print(f"Izvještaj za zgradu s ID-om {zgrada_id}:")
+    print("ID\tŠifra korisnika\tŠifra MM\tIme i prezime\tPotrošnja HV\tPotrošnja SV\tRazlika")
 
-#dohvaćanje podataka o korisnicima za odabrano razdoblje obračuna i zgradu
-cursor.execute('''
-    SELECT Korisnik.ID_korisnik, Korisnik.Vod_sif_kor, Korisnik.Vod_mm, Korisnik.Ime, Korisnik.Prezime, Obracun.Potrosnja_hv
-    FROM Korisnik
-    JOIN Obracun ON Korisnik.ID_korisnik = Obracun.ID_korisnik
-    WHERE Korisnik.ID_zgrada = ?
-    AND Obracun.Razdoblje_obracun = ?;
-''', (id_zgrada, razdoblje_obracun))
+    #dohvaćanje podataka o korisnicima za trenutnu zgradu i najnovije razdoblje obračuna
+    cursor.execute('''
+        SELECT Korisnik.Vod_ID, Korisnik.Vod_sif_kor, Korisnik.Vod_mm, Korisnik.Ime, Korisnik.Prezime, Obracun.Potrosnja_hv
+        FROM Korisnik
+        JOIN Obracun ON Korisnik.ID_korisnik = Obracun.ID_korisnik
+        WHERE Korisnik.ID_zgrada = ?
+        AND Obracun.Razdoblje_obracun = ?;
+    ''', (zgrada_id, najnovije_razdoblje))
 
-#ispis podataka o korisnicima
-for data in cursor.fetchall():
-    print(f"{data[0]}\t{data[1]}\t{data[2]}\t{data[3]} {data[4]}\t{data[5]}\t0\t0")
-
+    #ispis podataka o korisnicima
+    for data in cursor.fetchall():
+        print(f"{data[0]}\t{data[1]}\t{data[2]}\t{data[3]} {data[4]}\t{data[5]}\t0\t0")
+    print("\n")
 conn.close()

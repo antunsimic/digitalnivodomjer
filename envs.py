08bfddae8848c6172956369bf74ea96ed17ec458
Karlo Paić <karlo.paic102002@gmail.com>
2024-04-25 02:33:24 +0200

    Dodani try blokovi i premještanje nekih elemenata

--- envs.py
+++ envs.py
@@ -1,1 +1,4 @@
+SENDER_EMAIL = 'ritehvodomjer@gmail.com'
 APP_PASSWORD = 'sotb sfva lpms adqr'
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
--- python_google_gmail.py
+++ python_google_gmail.py
@@ -3,7 +3,7 @@ import smtplib
 #
 # envs.py dodatak
 #
-from envs import APP_PASSWORD
+from envs import SENDER_EMAIL, APP_PASSWORD, SMTP_SERVER, SMTP_PORT
 #from email.mime.text import MIMEText
 from email import encoders
 from email.mime.base import MIMEBase
@@ -14,54 +14,66 @@ from email.mime.multipart import MIMEMultipart
 conn = sqlite3.connect('vodomjeri.db')
 # curson
 cursor = conn.cursor()
-address = cursor.execute("SELECT Kontakt_email FROM Zgrada")
+rows = cursor.execute("SELECT Kontakt_email FROM Zgrada")
 
-sender = 'ritehvodomjer@gmail.com'
-#recipients = [sender, "dorotea376@gmail.com"]
+
 subject = 'Email subject'
 body = 'Neki tekst'
 
-
-with open("Neki_pdf.pdf", "rb") as attachment:
-    # Add the attachment to the message
-    part_pdf = MIMEBase("application", "octet-stream")
-    part_pdf.set_payload(attachment.read())
-encoders.encode_base64(part_pdf)
-part_pdf.add_header(
-    "Content-Disposition",
-    f"attachment; filename= Neki_pdf.pdf",
-)
-
-with open("Neki_excel.xlsx", "rb") as attachment:
-    # Add the attachment to the message
-    part_xlsx = MIMEBase("application", "octet-stream")
-    part_xlsx.set_payload(attachment.read())
-encoders.encode_base64(part_xlsx)
-part_xlsx.add_header(
-    "Content-Disposition",
-    f"attachment; filename= Neki_excel.xlsx",
-)
-
-
-for row in address:
+for row in rows:
     if row[0] is not None:
-
-        msg = MIMEMultipart()
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['To'] = '; '.join(row[0])
-        msg.attach(part_pdf)
-        msg.attach(part_xlsx)
-    
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-            smtp_server.login(sender, APP_PASSWORD)
-            smtp_server.sendmail(sender, row[0], msg.as_string())
-        print('Msg sent')
-
-  
-
-
-
-
-    
-    
+        
+        # dodavanje priloga mailu ako nisu nađeni ne šalje se mail
+        try:
+            with open("Neki_pdf.pdf", "rb") as attachment:
+                # Add the attachment to the message
+                part_pdf = MIMEBase("application", "octet-stream")
+                part_pdf.set_payload(attachment.read())
+                encoders.encode_base64(part_pdf)
+                part_pdf.add_header(
+                    "Content-Disposition",
+                    f"attachment; filename= Neki_pdf.pdf",
+                )
+        except FileNotFoundError:
+            print(f"PDF file not found")
+            continue
+        
+        try:
+            with open("Neki_excel.xlsx", "rb") as attachment:
+                # Add the attachment to the message
+                part_xlsx = MIMEBase("application", "octet-stream")
+                part_xlsx.set_payload(attachment.read())
+                encoders.encode_base64(part_xlsx)
+                part_xlsx.add_header(
+                    "Content-Disposition",
+                    f"attachment; filename= Neki_excel.xlsx",
+                )
+        except FileNotFoundError:
+            print(f"XLSX file not found")
+            continue
+            
+        addresses = row[0].split(";")
+        #iteriranje po adresama u jednom redu
+        for address in addresses:
+            print(address)
+            
+            msg = MIMEMultipart()
+            msg['Subject'] = subject
+            msg['From'] = SENDER_EMAIL
+            msg['To'] = address
+            msg.attach(part_pdf)
+            msg.attach(part_xlsx)
+            try:
+                with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp_server:
+                    smtp_server.login(SENDER_EMAIL, APP_PASSWORD)
+                    smtp_server.sendmail(SENDER_EMAIL, address, msg.as_string())
+                print(f'Email poruka je uspjesno poslana na {address}')
+            except smtplib.SMTPAuthenticationError:
+                print("SMTP Authentication Error")
+            except smtplib.SMTPException as e:
+                print(f"SMTP Exception: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+    else:
+        print("Email adresa zgrade nije dostupna")
+conn.close()

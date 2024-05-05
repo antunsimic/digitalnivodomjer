074e3bc4cd34ddf50aa52e21eb62b834b55c5e47
fmartinkovic <filip.martinkovic2@gmail.com>
2024-05-05 16:52:32 +0200

    Povezani backend i frontend
    Nije uspostavljena funkcionalnost logina.
    
    Više informacija u "procitati.txt"

---
+++ login-backend/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="/static/bundle.js"></script>
+    </body>
+    </html>
+    """
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if email == 'example@example.com' and password == 'password':
+            print(email)
+            print(password)
+            return jsonify({'success': True, 'message': 'Login successful'})
+        else:
+            print(email)
+            print(password)
+            return jsonify({'success': False, 'message': 'Login unsuccessful'})
+    else:
+        return jsonify({'success': False, 'message': 'Login Unsuccessful'}) #vraća se ovo, potrebno promijeniti react kod da koristi metodu POST?
+
+@app.route('/static/bundle.js') #u bundle.js su spojene sve komponente(App.js i LoginPage.js)
+def serve_bundle():
+    return send_from_directory('static', 'bundle.js')
+
+if __name__ == '__main__':
+    app.run(debug=True)

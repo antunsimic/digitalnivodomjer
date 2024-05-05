fe2256f8b53cb45e0efab237a081c13cfa9f4d9c
KarloP <95750952+KarloPaic@users.noreply.github.com>
2024-05-05 22:51:37 +0200

    Uređivanje funkcionalnosti i komentari

--- frontend/src/components/DatabasePage.tsx
+++ frontend/src/components/DatabasePage.tsx
@@ -8,8 +8,21 @@ import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react
 function DatabasePage() {
     const [file, setFile] = useState<File | null>(null);
     const [msg, setMsg] = useState(" ");
+    const [databaseAvailable, setDatabaseAvailable] = useState(false);
 
-    // handle za poziv upload funkcije
+    //postavak indikatora pri loadanju stranice
+    useEffect(() => {
+        axios.get('/database_availability')
+            .then(response => {
+                setDatabaseAvailable(response.data.database_available);
+            })
+            .catch(error => {
+                console.error('Error fetching database availability:', error);
+            });
+    }, []);
+
+
+    // handle za upload sa strane frontenda
     function handleUpload(){
         if(!file){
             setMsg("No file selected");
@@ -29,6 +42,8 @@ function DatabasePage() {
                 //povratne informacije s backenda
                 if(res.data.success){
                     setMsg(res.data.success);
+                    // postavi indikator na true ako je upload uspjesan
+                    setDatabaseAvailable(true);
                 }
                 else if(res.data.error){
                     setMsg(res.data.error);
@@ -37,17 +52,13 @@ function DatabasePage() {
                 console.log(res);
             })
             .catch(err => {
+                // u slucaju errora samog posta
                 setMsg("    Upload failed");
                 console.error(err);
             });
-
-        if('/database_availability'){
-            setMsg(" Baza je dostupna")
-        }else{
-            setMsg(" Baza nije postupna")
-        }
+        
     }
-// dodano za povezivanje A
+// handle za pozivanje preuzimanja baze
     const handleDownload = () => {
         axios({
             url: '/download',
@@ -63,11 +74,11 @@ function DatabasePage() {
                 document.body.appendChild(link);
                 link.click();
             } else {
-                // Handle other responses (e.g., error messages)
+                // ostali mogući odgovori (errori)
                 console.error(response.data);
             }
         }).catch(error => {
-            // Handle request errors
+            // error samog requesta
             console.error(error);
         });
     }
@@ -87,12 +98,9 @@ function DatabasePage() {
                     console.error('Error deleting uploaded file:', error);
                 });
         };
-
-        
         const cleanup = () => {
             handleNavigate();
         };
-
         return () => {
             cleanup();
         };
@@ -101,15 +109,18 @@ function DatabasePage() {
     // Brisanje po zatvaranju taba i refreshanju ali ne po promjeni stranice unutar webappa
     window.onbeforeunload = () => axios.delete('/delete');
 
-    
-    // A
+
     return(
 
          <div className="App">
-            {/*TESTIRANJE BRISANJA PO RUTIRANJU<div>
-                <Link to="/">home</Link>
-            </div>*/}
-             <div className = "submit">
+            
+            <div className = "submit">
+            {databaseAvailable ? (
+                <p>Database is uploaded and available.</p>
+                ) : (
+                <p>Database is not uploaded.</p>
+             )}
+            
              <input onChange={(e) => { 
                     const selectedFile = e.target.files && e.target.files[0];
                     if (selectedFile) {
@@ -127,12 +138,11 @@ function DatabasePage() {
              </div>
 
 
-
              <div className="prozor">
                  <p> </p>
                  <img src={er_dijagram} alt="ER DIJAGRAM BAZE PODATAKA" style={{ maxWidth: '100%' }}/>
              </div>
-
+             <br></br><br></br>
              <div className="download">
                  <button type="button" className="btn btn-primary" onClick={handleDownload}>Download</button>
              </div>

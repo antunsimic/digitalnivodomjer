e082e8ebd32ea15799cd0605c1ebeeac23b2e077
antunsimic <95712724+antunsimic@users.noreply.github.com>
2024-05-18 06:41:24 +0200

    Web stranica s funkcionalnim sesijama

---
+++ web_stranica/vodomjer_web/src/components/Sidemenu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../assets/sidemenu.css';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
+const Sidemenu = () => {
+  return (
+    <div className="side-menu">
+      <ul>
+        <li><Link to="/">Učitavanje datoteka vodomjera/bankovnih izvoda</Link></li>
+        <li><Link to="/izvjestaj">Formiranje izvještaja</Link></li>
+        <li><Link to="/slanje">Slanje izvještaja</Link></li>
+        <li><Link to="/baza">Uvid u bazu podataka</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidemenu;

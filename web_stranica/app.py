725c7eb909fe5f846f2450a0d434caadea4da249
Karlo Paić <karlo.paic102002@gmail.com>
2024-05-18 11:10:47 +0200

    Dohvacanje u 1 f, ispravci

--- web_stranica/app.py
+++ web_stranica/app.py
@@ -5,14 +5,17 @@ Created on Mon May 13 01:17:41 2024
 @author: antun_81f2caf
 """
 
-from flask import Flask, request, render_template, send_file, jsonify
+from flask import Flask, request, render_template, send_file, jsonify, session
 from bazaPodatakaFunct import upload_db, download_db, delete_db, vodomjeri_availability
-from godisnjaPotrosnjaFunct import get_consumption, get_godine, get_korisnici, get_zgrade
+from godisnjaPotrosnjaFunct import get_consumption, get_godine, get_korisnici, get_zgrade, get_filter_data
 import atexit
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+app.secret_key = 'your_really_secret_key_here'
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SECURE'] = True  # Use if HTTPS is enabled
 # treba spojit u file gdje su i ostale rute
 
 # ruta koja obavlja upload slike er
@@ -36,53 +39,81 @@ def database_availability():
     return vodomjeri_availability()
 
 # brisanje baze po isključivanju flask backenda
-atexit.register(delete_db)
+#atexit.register(delete_db)
 
+@app.route('/check-login-status')
+def check_login_status():
+    # Assume `logged_in` is a key in the session that is True when the user is logged in
+    return jsonify(logged_in=session.get('logged_in', False))
 
-def create_file(email, password):
-    file_path = 'podaci.txt'
-    with open(file_path, 'w') as f:
-        f.write(f"{email}\n{password}")
-
-def delete_file():
-    file_path = 'podaci.txt'
-    if os.path.exists(file_path):
-        os.remove(file_path)
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.json
+    data = request.get_json()  # Ensure you're correctly parsing JSON data
+    print("Received data:", data)
+    if session.get("logged_in"):
+        return jsonify({'error': 'Already logged in'}), 400
     email = data.get('email')
     password = data.get('password')
-    create_file(email, password)
+    if not email or not password:
+        return jsonify({'error': 'Email or password not entered'}), 400
+    
+    # For now, assume validation passes:
+    session["email"] = email
+    session["password"] = password
+    session["logged_in"] = True
     return jsonify({'success': True, 'message': 'Login successful'})
+
+
     
 @app.route('/logout')
 def logout():
-    delete_file()
-    return jsonify({'success': True, 'message': 'Logout successful'})
-
+    if session.get("logged_in"):
+        session.pop('logged_in', None)
+        session.pop('email', None)  # Consider also cleaning up other session data.
+        session.pop('password', None)
+        return jsonify({'success': True, 'message': 'Logout successful'})
+    else:
+        return "Već ste odjavljeni"
+    
 # godisnja-potrosnja
 #
+# vraćanje zgrada(ulica, mjesto, id) za prikaz na izborniku frontend
 @app.route('/get_zgrade', methods=['GET'])
 def get_buildings():
-    return get_zgrade()
-    
+    if session.get("logged_in") and session.get("uploaded_file"):
+        return get_zgrade()
+    else:
+        print("Baza nije uploadana ili korisnik nije ulogiran")
+# vraćanje korisnika(id, ime, prezime) za prikaz na izborniku frontend    
 @app.route('/get_korisnici', methods=['GET'])
 def get_users():
-    return get_korisnici()
-    
-
+    if session.get("logged_in") and session.get("uploaded_file"):
+        return get_korisnici()
+    else:
+        print("Baza nije uploadana ili korisnik nije ulogiran")
+# vraćanje godina(dubstring prva 4 broja razdoblja) za prikaz na izborniku frontend
 @app.route('/get_godine', methods=['GET'])
 def get_years():
-    return get_godine()
-   
-
+    if session.get("logged_in") and session.get("uploaded_file"):
+        return get_godine()
+    else:
+        print("Baza nije uploadana ili korisnik nije ulogiran")
+# kombinacija prethodne tri - odabrati koja opcija je bolja za frontend
+@app.route('/get_filter', methods=['GET'])
+def get_filters():
+    if session.get("logged_in") and session.get("uploaded_file"):
+        return get_filter_data()
+    else:
+        print("Baza nije uploadana ili korisnik nije ulogiran")
+# vraćanje podataka dobivenih na         
 @app.route('/potrosnja', methods=['GET'])
 def get_consumption_data():
-    return get_consumption()
-    
+    if session.get("logged_in") and session.get("uploaded_file"):
+        return get_consumption()
+    else:
+        print("Baza nije uploadana ili korisnik nije ulogiran")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web_stranica/godisnjaPotrosnjaFunct.py
+++ web_stranica/godisnjaPotrosnjaFunct.py
@@ -1,15 +1,14 @@
 from flask import request, jsonify
 import sqlite3
 
-
-
 # Nalazi id, ulice i mjesta zgrada u Zgrada tablici i vraća ih za odabir na frontendu (ulica i mjesto kao tekst, id bi se trebao vratit po odabiru)
 def get_zgrade():
     conn = sqlite3.connect('web_stranica/datoteke/vodomjeri.db')
     cursor = conn.cursor()
-    buildings =cursor.execute('SELECT ID_zgrada, Ulica_kbr, Mjesto FROM Zgrada').fetchall()
+    buildings = cursor.execute('SELECT ID_zgrada, Ulica_kbr, Mjesto FROM Zgrada').fetchall()
     conn.close()
     return jsonify(buildings=[{'id': building[0], 'ulica': building[1], 'mjesto': building[2]} for building in buildings])
+
 # Nalazi id, ime i prezime korisnika i vraća za ofabir na frontendu
 def get_korisnici():
     conn = sqlite3.connect('web_stranica/datoteke/vodomjeri.db')
@@ -17,6 +16,7 @@ def get_korisnici():
     users = cursor.execute('SELECT ID_korisnik, Ime, Prezime FROM Korisnik').fetchall()    
     conn.close()
     return jsonify(users=[{'id': user[0], 'ime': user[1], 'prezime': user[2]} for user in users])
+
 # Nalazi godinu za odabir na frontendu
 def get_godine():
     conn = sqlite3.connect('web_stranica/datoteke/vodomjeri.db')
@@ -25,6 +25,29 @@ def get_godine():
     years = cursor.execute('SELECT DISTINCT substr(Razdoblje_obracun, 1, 4) FROM Obracun').fetchall()
     conn.close()
     return jsonify(years=[year[0] for year in years])
+
+# kombinirana funkcija prethodne tri - ODABRATI koja je bolja za frontend ostalo izbrisat
+def get_filter_data():
+    conn = sqlite3.connect('web_stranica/datoteke/vodomjeri.db')
+    cursor = conn.cursor()
+    
+    # dohvaćanje podataka za odbir na frontendu
+    buildings =cursor.execute('SELECT ID_zgrada, Ulica_kbr, Mjesto FROM Zgrada').fetchall()
+    users = cursor.execute('SELECT ID_korisnik, Ime, Prezime FROM Korisnik').fetchall() 
+    years = cursor.execute('SELECT DISTINCT substr(Razdoblje_obracun, 1, 4) FROM Obracun').fetchall()
+    conn.close()
+
+    # Uređivanje podataka za vraćanje na prontend
+    buildings_data = [{'id': building[0], 'ulica': building[1], 'mjesto': building[2]} for building in buildings]
+    users_data = [{'id': user[0], 'ime': user[1], 'prezime': user[2]} for user in users]
+    years_data = [year[0] for year in years]
+    
+    #print("Buildings:", buildings)
+    #print("Users:", users) 
+    #print("Years:", years)
+    
+    return jsonify(buildings=buildings_data, users=users_data, years=years_data)
+
 # Na temelju odabranog(ID_zgrada, ID_korisnik, godina) uzima vrijednosti potrošnje i datume obračuna
 def get_consumption():
     conn = sqlite3.connect('web_stranica/datoteke/vodomjeri.db')
@@ -33,7 +56,7 @@ def get_consumption():
     user = request.args.get('user')
     year = request.args.get('year')
 
-    result = cursor.execute(f"SELECT Datum_obracun, Potrosnja_hv FROM Obracun WHERE ¸ID_zgrada='{building}' AND ID_korisnik={user} AND razdoblje_obracun LIKE '{year}%'").fetchall()
+    result = cursor.execute(f"SELECT Datum_obracun, Potrosnja_hv FROM Obracun WHERE ¸ID_zgrada='{building}' AND ID_korisnik={user} AND razdoblje_obracun LIKE '{year}%' ORDER BY Datum_obracun ASC").fetchall()
     conn.close()
     # Vrijednosti koje se vracaju za graf 
     consumption = [{'datum': row[0], 'potrosnja': row[1]} for row in result]

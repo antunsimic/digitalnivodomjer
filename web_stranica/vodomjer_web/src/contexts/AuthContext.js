ee828ee13e50b489dab80925912b11752f9fc356
antunsimic <95712724+antunsimic@users.noreply.github.com>
2024-05-19 01:25:34 +0200

    Update AuthContext.js
    izbacivanje redundantnog dijela koda (taj posao već obavlja AuthStatusChecker.js)

--- web_stranica/vodomjer_web/src/contexts/AuthContext.js
+++ web_stranica/vodomjer_web/src/contexts/AuthContext.js
@@ -6,20 +6,6 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const fetchLoginStatus = async () => {
-      try {
-        const response = await axios.get('/check-login-status', { withCredentials: true });
-        setLoggedIn(response.data.logged_in);
-      } catch (error) {
-        console.error('Failed to fetch login status:', error);
-      }
-    };
-
-    fetchLoginStatus();
-  }, []);
-
   return (
     <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
       {children}

491907c1fc81564eb3024663d6436ca224bbeaa6
KarloP <95750952+KarloPaic@users.noreply.github.com>
2024-05-20 12:17:36 +0200

    Ograničenja odabira korisnika i godine

--- web_stranica/godisnjaPotrosnjaFunct.py
+++ web_stranica/godisnjaPotrosnjaFunct.py
@@ -1,14 +1,16 @@
 from flask import request, jsonify, session
 import sqlite3
 
-
 def connect_to_db():
     filepath = session.get("uploaded_file")
     print(filepath)
     #conn = sqlite3.connect('web_stranica/datoteke/vodomjeri.db')
-    conn = sqlite3.connect(filepath)    ########################testirat jos
+    conn = sqlite3.connect(filepath)    
     cursor = conn.cursor()
     return conn, cursor
+
+#### TREBALO BI NAMJESTIT DA TE PRVO ODABIRE ZGRADA PA SE TADA NA ON CHANGE DOHVACAJU DOSPIPNI KORISNICI,
+#### KADA SE ODABRA KORISNIKA NA ON CHANGE SSE DOHVATE GODINE
 # Nalazi id, ulice i mjesta zgrada u Zgrada tablici i vraća ih za odabir na frontendu (ulica i mjesto kao tekst, id bi se trebao vratit po odabiru)
 def get_zgrade():
     conn, cursor = connect_to_db()
@@ -19,18 +21,28 @@ def get_zgrade():
 # Nalazi id, ime i prezime korisnika i vraća za ofabir na frontendu
 def get_korisnici():
     conn, cursor = connect_to_db()
-    users = cursor.execute('SELECT ID_korisnik, Ime, Prezime FROM Korisnik').fetchall()    
+    selected_building = request.args.get('selected_building')
+    # odabir korisnika s obzirom koja je zgrada odabrana
+    users = cursor.execute('''
+        SELECT ID_korisnik, Ime, Prezime 
+        FROM Korisnik 
+        WHERE ID_zgrada = ?
+    ''', (selected_building,)).fetchall()
+   
     conn.close()
     return jsonify(users=[{'id': user[0], 'ime': user[1], 'prezime': user[2]} for user in users])
 
 # Nalazi godinu za odabir na frontendu
 def get_godine():
     conn, cursor = connect_to_db()
-    # substring koji iz Razdoblje_obracun uzima iskljucivo godinu
-    years = cursor.execute('SELECT DISTINCT substr(Razdoblje_obracun, 1, 4) FROM Obracun').fetchall()
+    selected_user = request.args.get('selected_user')
+    # substring koji iz Razdoblje_obracun uzima iskljucivo godinu, na temelju koji je user prethodno uzet
+    years = cursor.execute('''SELECT DISTINCT substr(Razdoblje_obracun, 1, 4) FROM Obracun WHERE ID_zgrada = ?
+    ''', (selected_user,)).fetchall()
     conn.close()
     return jsonify(years=[year[0] for year in years])
 
+### NE VRIJEDE OGRANIČENJA REDOSLIJEDA ODABIRA
 # kombinirana funkcija prethodne tri - ODABRATI koja je bolja za frontend ostalo izbrisat
 def get_filter_data():
     conn, cursor = connect_to_db()
@@ -59,10 +71,12 @@ def get_consumption():
     user = request.args.get('user')
     year = request.args.get('year')
 
-    result = cursor.execute(f"SELECT Datum_obracun, Potrosnja_hv FROM Obracun WHERE ¸ID_zgrada='{building}' AND ID_korisnik={user} AND razdoblje_obracun LIKE '{year}%' ORDER BY Datum_obracun ASC").fetchall()
+    result = cursor.execute(f"SELECT Datum_obracun, Potrosnja_hv FROM Obracun WHERE ID_korisnik={user} AND razdoblje_obracun LIKE '{year}%' ORDER BY Datum_obracun ASC").fetchall()
     conn.close()
     # Vrijednosti koje se vracaju za graf 
     consumption = [{'datum': row[0], 'potrosnja': row[1]} for row in result]
 
 
     return jsonify({'consumption': consumption})
+
+    
